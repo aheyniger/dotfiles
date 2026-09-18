0136359a0aaa195dbaf497b82097b3240b1928f5
Anders Heyniger <anders.heyniger@gmail.com>
2026-09-18 16:35:29 -0400

    added zsh and starship

--- Documents/Powershell/Microsoft.PowerShell_profile.ps1
+++ Documents/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,26 @@
-# Custom prompt: [time] user@host path (git-branch)
-#                $
+if (Get-Command starship -ErrorAction SilentlyContinue) {
+    Invoke-Expression (&starship init powershell)
+} else {
+    # Fallback custom prompt: [time] user@host path (git-branch)
+    #                         $
+    function prompt {
+        $esc = [char]27
+        $reset   = "$esc[0m"
+        $lpurple = "$esc[95m"
+        $blue    = "$esc[34m"
+        $lblue   = "$esc[94m"
+        $purple  = "$esc[35m"
 
-function prompt {
-    $esc = [char]27
-    $reset   = "$esc[0m"
-    $lpurple = "$esc[95m"
-    $blue    = "$esc[34m"
-    $lblue   = "$esc[94m"
-    $purple  = "$esc[35m"
+        $shell    = 'PS'
+        $time     = Get-Date -Format 'HH:mm:ss'
+        $userHost = "$env:USERNAME@$env:COMPUTERNAME"
+        $path     = (Get-Location).Path
 
-    $shell    = 'PS'
-    $time     = Get-Date -Format 'HH:mm:ss'
-    $userHost = "$env:USERNAME@$env:COMPUTERNAME"
-    $path     = (Get-Location).Path
+        $branch = ''
+        $b = git symbolic-ref --short HEAD 2>$null
+        if (-not $b) { $b = git rev-parse --short HEAD 2>$null }
+        if ($b) { $branch = " ($b)" }
 
-    $branch = ''
-    $b = git symbolic-ref --short HEAD 2>$null
-    if (-not $b) { $b = git rev-parse --short HEAD 2>$null }
-    if ($b) { $branch = " ($b)" }
-
-    "$lpurple$shell [$time]$reset $blue$userHost$reset $lblue$path$reset$purple$branch$reset`n${blue}`$ $reset"
+        "$lpurple$shell [$time]$reset $blue$userHost$reset $lblue$path$reset$purple$branch$reset`n${blue}`$ $reset"
+    }
 }
